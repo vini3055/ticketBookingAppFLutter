3cad25a6c75ca4390c3b6d17fd1ced03ec607c1c
vini3055 <kvineethvini94@gmail.com>
2024-05-16 12:10:25 +0530

    images added

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,6 +1,7 @@
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ticketapp/screens/home_screen.dart';
 
 class BottomBar extends StatefulWidget {
   const BottomBar({super.key});
@@ -11,46 +12,58 @@ class BottomBar extends StatefulWidget {
 
 class _BottomBarState extends State<BottomBar> {
   int _variableSelectedIndex = 0;
-static final List<Widget>_widgetOptions = <Widget>[
-const Text("Home"),
-const Text("Search"),
-const Text("Tickets"),
-const Text("Profile"),
+  static final List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    const Text("Search"),
+    const Text("Tickets"),
+    const Text("Tickets"),
+    // HomeScreen()
+  ];
 
-];
-void _onItemTapped(int index){
-  // setState is to get updated realtime
-  setState(() {
-    _variableSelectedIndex = index;
-  });
-print('${index}');
-}
+  void _onItemTapped(int index) {
+    // setState is to get updated realtime
+    setState(() {
+      _variableSelectedIndex = index;
+    });
+    print('${index}');
+  }
 
   @override
   Widget build(BuildContext context) {
-
-
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: Text('My ticketApp'),
+        title: Text('My ticketApp', style: TextStyle(color: Colors.white)),
       ),
       body: Center(
-          child:_widgetOptions[_variableSelectedIndex],
+        child: _widgetOptions[_variableSelectedIndex],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        onTap:_onItemTapped ,
+        currentIndex: _variableSelectedIndex,
+        onTap: _onItemTapped,
         elevation: 10,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
         selectedItemColor: Colors.blue,
+        type: BottomNavigationBarType.fixed,
         unselectedItemColor: const Color(0xFF526480),
         items: [
-          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular), activeIcon:Icon(FluentSystemIcons.ic_fluent_home_regular), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_search_filled), activeIcon:Icon(FluentSystemIcons.ic_fluent_search_filled), label: "Search"),
-          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular), activeIcon:Icon(FluentSystemIcons.ic_fluent_ticket_regular), label: "Ticket"),
-          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_person_filled), activeIcon:Icon(FluentSystemIcons.ic_fluent_person_filled), label: "User"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              label: "Search"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              label: "Ticket"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              label: "User"),
         ],
       ),
     );
